34d6990353417c88d51d61ab9f98479cc79f597c
01GlennAM <shunretrofied@gmail.com>
2026-08-23 14:35:07 -0500

    se agrega  registro de notas

--- main.py
+++ main.py
@@ -15,3 +15,51 @@ def registrar_ingreso():
         estudiantes[nombre] = []
         print(f"Estudiante {nombre} registrado correctamente.")
 
+# Registra una nota a un estudiante existente.
+    
+def registrar_nota():
+   
+
+    nombre = input("Ingrese el nombre del estudiante: ")
+
+    if nombre in estudiantes:
+
+        try:
+            nota = float(input("Ingrese la nota: "))
+
+            if 0 <= nota <= 5:
+                estudiantes[nombre].append(nota)
+                print("Nota registrada correctamente.")
+            else:
+                print("La nota debe estar entre 0 y 5.")
+
+        except ValueError:
+            print("Debe ingresar un número válido.")
+
+    else:
+        print("El estudiante no está registrado.")
+
+
+# Calcula y muestra el promedio de un estudiante.
+
+def ver_promedio():
+    
+    nombre = input("Ingrese el nombre del estudiante: ")
+
+    if nombre in estudiantes:
+
+        notas = estudiantes[nombre]
+
+        if len(notas) > 0:
+
+            promedio = sum(notas) / len(notas)
+
+            print(f"\nEstudiante: {nombre}")
+            print(f"Notas: {notas}")
+            print(f"Promedio: {promedio:.2f}")
+
+        else:
+            print("El estudiante todavía no tiene notas.")
+
+    else:
+        print("El estudiante no está registrado.")

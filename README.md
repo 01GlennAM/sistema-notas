e9e1d85d674d5aa15e315be887d3e1d6900d3481
01GlennAM <shunretrofied@gmail.com>
2026-08-23 14:44:57 -0500

    se agrega verificar

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 <!--se usa para cambiar a la rama main / git branch -M main-->
+<!-- cambia para la rama main git branch -M main-->
+
+<!-- se sube develop al github con git push -u origin develop -->
+
+<!-- creamos feature/notas con el comando git switch -c feature/notas --> 
+<!-- se crea desde develop -->
+<!-- se crea la logica  -->
+
+<!-- creamos feature/prueba con git switch -c feature/aprueba -->
+ <!-- acá se hacen las pruebas  -->
+
+<!-- para hacer subir git a push -u origin feature/aprueba -->
+
+<!-- git status vemos los cambios en las ramas -->
--- main.py
+++ main.py
@@ -15,3 +15,32 @@ def registrar_ingreso():
         estudiantes[nombre] = []
         print(f"Estudiante {nombre} registrado correctamente.")
 
+
+# Verifica si el estudiante aprobó o reprobó.
+def verificar_aprobacion():
+    
+    nombre = input("Ingrese el nombre del estudiante: ")
+
+    if nombre in estudiantes:
+
+        notas = estudiantes[nombre]
+
+        if len(notas) > 0:
+
+            promedio = sum(notas) / len(notas)
+
+            print(f"\nPromedio de {nombre}: {promedio:.2f}")
+
+            if promedio >= 3.0:
+                print("Estado: APROBADO")
+            else:
+                print("Estado: REPROBADO")
+
+        else:
+            print("El estudiante no tiene notas registradas.")
+
+    else:
+        print("El estudiante no está registrado.")
+
+
+
